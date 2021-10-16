aca40259fa0c363acd27aaa0010073a63eb239c3
micha vie <micha@freeiam.com>
2021-10-16 18:54:11 +0200

    fix download aliasing

--- install.sh
+++ install.sh
@@ -2,51 +2,51 @@
 
 echo "Setting up your Mac..."
 
-# Check for Homebrew and install if we don't have it
+# check for homebrew and install if we don't have it
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-# Update Homebrew recipes
+# update homebrew recipes
 brew update
 
-# Install all our dependencies with bundle (See Brewfile)
+# install all our dependencies with bundle (see brewfile)
 cd ~/.dotfiles
 brew tap homebrew/bundle
 brew bundle
 
-# Set default MySQL root password and auth type
+# set default mysql root password and auth type
 mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'root'; FLUSH PRIVILEGES;"
 
-# Install PHP extensions with PECL
+# install php extensions with pecl
 pecl install imagick memcached redis swoole
 
-# Install global Composer packages
+# install global composer packages
 /usr/local/bin/composer global require laravel/valet
 /usr/local/bin/composer global require beyondcode/expose
 /usr/local/bin/composer global require laravel/vapor-cli
 /usr/local/bin/composer global require laravel/forge-cli
 
-# Create a Sites directory
+# create a sites directory
 mkdir $HOME/Code
 
-# Install Laravel Valet
+# install laravel valet
 $HOME/.composer/vendor/bin/valet install
 $HOME/.composer/vendor/bin/valet park $HOME/Code
 
-# Clone Github repositories
+# clone github repositories
 ./clone.sh
 
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+# removes .zshrc from $home (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
-# Symlink the Mackup config file to the home directory
+# symlink the mackup config file to the home directory
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
-# Removes Downloads folder and symlinks the iClouds Download folder
-rm -r $HOME/Downloads
+# removes downloads folder and symlinks the iclouds download folder
+sudo rm -r $HOME/Downloads
 ln -s $HOME/Library/Mobile\ Documents/com~apple~CloudDocs/Downloads $HOME/Downloads
 
-# Set macOS preferences - we will run this last because this will reload the shell
+# set macos preferences - we will run this last because this will reload the shell
 source .macos
