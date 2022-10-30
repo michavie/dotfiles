49b3b07fad98ae505c215a3db588970c63291b00
micha vie <micha@freeiam.com>
2022-10-30 12:15:48 +0100

    update clonables

--- clone.sh
+++ clone.sh
@@ -8,9 +8,8 @@ CODE=$HOME/Code
 git clone git@github.com:michavie/michavie.com.git $CODE/michavie.com
 
 # superciety
-git clone git@github.com:Superciety/pwa-core-library.git $CODE/superciety--pwa-core-library
 git clone git@github.com:Superciety/elrond-sdk-laravel.git $CODE/superciety--elrond-sdk-laravel
-git clone git@github.com:Superciety/front-pwa.git $CODE/superciety--front-pwa
+git clone git@github.com:Superciety/front-pwa.git $CODE/superciety--pwa
 git clone git@github.com:Superciety/api.git $CODE/superciety--api
 git clone git@github.com:Superciety/identity-sc.git $CODE/superciety--identity-sc
 git clone git@github.com:Superciety/fellowships-sc.git $CODE/superciety--fellowships-sc
