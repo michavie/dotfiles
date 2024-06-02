f287293977dd2ba09102f672d2cae8020727e423
Micha Vie <micha@vleap.io>
2024-06-02 11:54:58 +0200

    update repos

--- dev.sh
+++ dev.sh
@@ -19,14 +19,25 @@ npm install -g ts-node
 npm install -g npm-check-updates
 
 # cloning repos
-git clone git@github.com:michavie/michavie.com.git $CODE/michavie.com
-
 git clone git@github.com:PeerMeHQ/dao-sc $CODE/peerme--dao-sc
 git clone git@github.com:PeerMeHQ/dao-plug-template-sc $CODE/peerme--dao-plug-template-sc
 git clone git@github.com:PeerMeHQ/identity-sc $CODE/peerme--identity-sc
 git clone git@github.com:PeerMeHQ/snippets $CODE/peerme--snippets
-git clone git@github.com:PeerMeHQ/mx-sdk-php $CODE/peerme--mx-sdk-php
 git clone git@github.com:PeerMeHQ/mx-sdk-laravel $CODE/peerme--mx-sdk-laravel
-git clone git@github.com:PeerMeHQ/mx-sdk-php-network-providers $CODE/peerme--mx-sdk-php-network-providers
 git clone git@github.com:PeerMeHQ/extensions $CODE/peerme--extensions
 git clone git@github.com:PeerMeHQ/know $CODE/peerme--know
+git clone git@github.com:PeerMeHQ/mx-strategy-contribution-token-sc.git $CODE/peerme--strategy-contribution-token-sc
+git clone git@github.com:PeerMeHQ/mx-strategy-contribution-tokenless-sc.git $CODE/peerme--strategy-contribution-tokenless-sc
+git clone git@github.com:PeerMeHQ/earn-sc.git $CODE/peerme--earn-sc
+git clone git@github.com:PeerMeHQ/bounties-sc.git $CODE/peerme--bounties-sc
+
+git clone git@github.com:SpawnableHQ/api.git $CODE/spawnable--api
+git clone git@github.com:SpawnableHQ/pwa.git $CODE/spawnable--pwa
+git clone git@github.com:SpawnableHQ/bridge-mx.git $CODE/spawnable--bridge-mx
+git clone git@github.com:SpawnableHQ/mx-spawner-sc.git $CODE/spawnable--mx-spawner-sc
+
+git clone git@github.com:VirtualLeap/organization-sc.git $CODE/vleap--organization-sc
+git clone git@github.com:VirtualLeap/legal.git $CODE/vleap--legal
+git clone git@github.com:VirtualLeap/api.git $CODE/vleap--api
+git clone git@github.com:VirtualLeap/pwa.git $CODE/vleap--pwa
+git clone git@github.com:VirtualLeap/hub.git $CODE/vleap--hub
